c91bd540cc1d6017cc1416fbd89bd04cf3f9600a
U-FRANCESCA\molt <Paul.Golds+normalvector@GMail.com>
2017-03-22 21:41:07 +0000

    Started to refactor into MeshDeformationComponent - it can now read meshes.

---
+++ Plugins/ProceduralToolkit/Source/ProceduralToolkit/Private/MeshDeformationComponent.cpp
@@ -0,0 +1,31 @@
+// (c)2017 Paul Golds, released under MIT License.
+
+#include "ProceduralToolkit.h"
+#include "MeshGeometry.h"
+#include "MeshDeformationComponent.h"
+
+
+// Sets default values for this component's properties
+UMeshDeformationComponent::UMeshDeformationComponent()
+{
+	// This component can never tick, it doesn't update itself.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+
+bool UMeshDeformationComponent::LoadFromStaticMesh(UStaticMesh *staticMesh, int32 LOD /*= 0*/)
+{
+	// If there's no static mesh we have nothing to do..
+	if (!staticMesh) {
+		UE_LOG(LogTemp, Warning, TEXT("LoadFromStaticMesh: No StaticMesh provided"));
+		meshGeometry = nullptr;
+		return false;
+	}
+
+	UE_LOG(LogTemp, Log, TEXT("Reading mesh geometry from static mesh '%s'"), *staticMesh->GetName());
+	meshGeometry = nullptr;
+
+	return false;
+}
---
+++ Plugins/ProceduralToolkit/Source/ProceduralToolkit/Public/MeshDeformationComponent.h
@@ -0,0 +1,28 @@
+// (c)2017 Paul Golds, released under MIT License.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "MeshGeometry.h"
+#include "MeshDeformationComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class PROCEDURALTOOLKIT_API UMeshDeformationComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UMeshDeformationComponent();
+
+	// This is the mesh geometry currently stored within the component
+	UPROPERTY(BlueprintReadonly, Category = MeshDeformationComponent)
+		UMeshGeometry *meshGeometry = nullptr;
+
+
+	// Pull out the geometry of the mesh so we're able to modify it
+	UFUNCTION(BlueprintCallable, Category = MeshDeformationComponent)
+		bool LoadFromStaticMesh(UStaticMesh *staticMesh, int32 LOD = 0);
+	
+};
