41d46c5c299527835c1abbd64916d48280ddc558
Paul Golds <Paul.Golds+normalvector@GMail.com>
2017-05-31 15:46:46 +0100

    Now have SelectionSet.h documented.

--- Plugins/ProceduralToolkit/Source/ProceduralToolkit/Public/SelectionSet.h
+++ Plugins/ProceduralToolkit/Source/ProceduralToolkit/Public/SelectionSet.h
@@ -6,30 +6,46 @@
 #include "Kismet/KismetMathLibrary.h"
 #include "SelectionSet.generated.h"
 
-/**
- * 
- */
+/// This stores a set of weightings for a selection set.
+///
+/// The initial use for this is to provide the vertex weightins for *MeshGeometry*, but
+/// it should be possible to use this for other selection weightings in the future.
+///
+/// The *SelectionSetBPLibrary* contains a lot of helper functions for this which allow
+/// selection sets to be modified.
 UCLASS(BlueprintType)
 class PROCEDURALTOOLKIT_API USelectionSet : public UObject
 {
 	GENERATED_BODY()
 	
 public:
-	// The set of weights this set has.
+	/// The weights this set contains.
 	UPROPERTY(BlueprintReadWrite, Category = SelectionSet)
 		TArray<float> weights;
-
-	// Populates the set with the supplied number weights 
+	
+	/// Creates a selection set of the size provided with zero weights.
+	/// \param size			The number of items in the selection set
 	void CreateSelectionSet(int32 size);
 
-	// Empties the set
+	/// Empties the set, setting the size to zero.
 	void Empty();
 
-	// Set all values to same value
+	/// Sets the weight of all values in the selection set to the weight provided.
+	///
+	/// This will preserve the number of elements in the set, only the values will change.
+	///
+	/// \param weight		The weight to assign to all values
 	UFUNCTION(BlueprintCallable, Category = SelectionSet)
 		USelectionSet *SetAllWeights(float weight);
 
-	// Randomize the weights of the selection set
+	/// Randomize the weights of the selection set
+	///
+	/// This will preserve the number of elements in the set, only the values will change.
+	///
+	/// \param randomStream			The RandomStream to use for the source
+	/// \param minWeight			The minimum value of the random weightings
+	/// \param maxWeight			The maximum value of the random weightings
+
 	UFUNCTION(BlueprintCallable, Category = SelectionSet)
 		USelectionSet *RandomizeWeights(FRandomStream randomStream, float minWeight = 0, float maxWeight = 1);
 };
